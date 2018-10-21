9c9c4933e540ccac2010a67e085c4532eab229c3
Long Vo <vovanlong1997@gmail.com>
2018-10-21 22:56:47 +0700

    nodtification

--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -7,5 +7,6 @@ const AuthHelper = require('../Helpers/AuthHelper');
 
 router.post('/follow-user', AuthHelper.VerifyToken, FriendCtr.FollowUser);
 router.post('/unfollow-user', AuthHelper.VerifyToken, FriendCtr.UnFollowUser);
+router.post('/max/:id', AuthHelper.VerifyToken, FriendCtr.MaxNotification);
 
 module.exports = router;
