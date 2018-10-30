c3f14fc8a383e99bdef7cb9f475721dc37c7cd48
Long Vo <vovanlong1997@gmail.com>
2018-10-31 05:35:47 +0700

    profile view

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status-codes');
-
+const moment = require('moment');
 const User = require('../models/userModels');
 
 module.exports = {
@@ -52,13 +52,37 @@ module.exports = {
         res.status(httpStatus.OK).json({ message: 'User by username', result });
       })
       .catch(err => {
-        res
-          .status(httpStatus.INTERNAL_SERVER_ERROR)
-          .json({ message: 'Error occured' });
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: err });
       });
   },
 
   async ProfileView(req, res) {
-    console.log(req.body);
+    const date = moment().format('YYYY-MM-DD');
+    await User.update(
+      {
+        _id: req.body.id,
+        'notifications.data': { $ne: [date, ''] },
+        'notifications.senderId': { $ne: req.user._id }
+      },
+      {
+        $push: {
+          notifications: {
+            senderId: req.user._id,
+            message: `${req.user.username} View your profile`,
+            created: new Date(),
+            data: date,
+            viewProfile: true
+          }
+        }
+      }
+    )
+      .then(result => {
+        res.status(httpStatus.OK).json({ message: 'Notification sent' });
+      })
+      .catch(err => {
+        res
+          .status(httpStatus.INTERNAL_SERVER_ERROR)
+          .json({ message: 'Error occured' });
+      });
   }
 };
